0518294ab2af3421ec21a7e8fd08aff1be0ad7ad
AllanJRY <jarry.allan@gmail.com>
2021-06-16 19:24:35 +0200

    [FEAT] Created middleware to add session token in any request headers

--- Client/src/index.js
+++ Client/src/index.js
@@ -6,11 +6,26 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient, { InMemoryCache, gql } from 'apollo-boost';
+import ApolloClient, { InMemoryCache, gql, HttpLink, ApolloLink, concat } from 'apollo-boost';
+
+const httpLink = new HttpLink({ uri: '/graphql' });
+
+const tryAppendSessionTokenToHeaders = new ApolloLink((operation, forward) => {
+    // add the authorization to the headers
+    operation.setContext(({ headers = {} }) => ({
+        headers: {
+            ...headers,
+            authorization: localStorage.getItem('session_token') || null,
+        }
+    }));
+
+    return forward(operation);
+});
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000/',
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    link: concat(tryAppendSessionTokenToHeaders, httpLink),
 });
 
 client.query({
@@ -24,11 +39,11 @@ client.query({
 
 ReactDOM.render(
     <BrowserRouter>
-        <ApolloProvider client={ client }>
+        <ApolloProvider client={client}>
             <App />
         </ApolloProvider>
     </BrowserRouter>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
 
 serviceWorker.unregister();
